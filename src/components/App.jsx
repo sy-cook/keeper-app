d17f8c803b8d5d03840df25885665385ab23b43e
sy-cook <seyoung.cook@gmail.com>
2022-01-12 21:45:40 -0800

    Added functionality to "add" and "delete" buttons.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
-import notes from "../notes";
+// import notes from "../notes";
 import CreateArea from "./CreateArea";
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  const [items, setItems] = useState([]);
+  
+  function addItem(note) {
+    const uniqueID = uuidv4();
+    const noteWithID = {id: uniqueID, ...note};
+
+    setItems((prevItems) => {
+      return [...prevItems, noteWithID]
+    });
+  }
+
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((noteItem) => {
+        return noteItem.id !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      {/* {notes.map((noteItem) => (
+      <CreateArea onAdd={addItem} />
+      {items.map((noteItem) => (
         <Note 
-					key={noteItem.key} 
+					key={noteItem.id} 
+          id={noteItem.id} 
 					title={noteItem.title} 
 					content={noteItem.content} 
+          onChecked={deleteItem}
 				/>
-      ))} */}
+      ))}
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+
+function CreateArea(props) {
+
+	const initialValues = {
+		title: "",
+		content: ""
+	};
+
+	const [values, setValues] = useState(initialValues);
+
+	
+	function handleInputChange(event) {
+			const { name, value } = event.target;
+			setValues({
+				...values,
+				[name]: value
+			});
+	};
+
+	function handleClick(event) {
+		props.onAdd(values);
+		setValues(initialValues);
+		event.preventDefault();
+	}
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form onSubmit={handleClick}>
+        <input onChange={handleInputChange} name="title" placeholder="Title" value={values.title} />
+        <textarea onChange={handleInputChange} name="content" placeholder="Take a note..." rows="3" value={values.content} />
+        <button type="submit">
+					Add
+				</button>
       </form>
     </div>
   );
